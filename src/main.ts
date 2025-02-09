305049be7f1fad444a8caf727cde1ef99eb4b982
Apmen03 <b.vardy03@gmail.com>
2025-02-09 13:16:21 +0400

    Trying to fix a bug

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.enableCors({
+    origin: "https://back-end.com.ge",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
