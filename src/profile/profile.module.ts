2ec59e3d342de1207d2890fcb2236cdda763a7ff
Apmen03 <b.vardy03@gmail.com>
2025-02-20 15:50:51 +0400

    fix bug

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]),UserModule],
+  imports: [TypeOrmModule.forFeature([User]),UserModule,EmailModule],
   controllers: [ProfileController],
   providers: [UserService],
 })
