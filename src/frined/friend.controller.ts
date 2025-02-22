1e6c7756c150ff924b807995227e5f467b27f564
Apmen03 <b.vardy03@gmail.com>
2025-02-22 17:21:52 +0400

    big update for friend module

--- src/frined/friend.controller.ts
+++ src/frined/friend.controller.ts
@@ -8,24 +8,28 @@ interface AuthenticatedRequest extends Request {
 }
 
 @Controller('friends')
+@UseGuards(JwtGuard)
 export class FriendController {
   constructor(private readonly friendService: FriendService) {}
+  @UseGuards(UserGuard)
   @Get(':id')
   findFriend(@Param('id') id: string) {
     return this.friendService.findFriend(+id);
   }
 
+  @UseGuards(UserGuard)
   @Post('request/:receiverId')
   sendFriendRequest(@Param('receiverId') receiverId: number, @Req() req: AuthenticatedRequest) {
     if (!req.user) throw new Error('User not found in request');
     return this.friendService.sendFriendRequest(req.user.id, receiverId);
   }
 
+  @UseGuards(UserGuard)
   @Post('accept/:requestId')
   acceptFriendRequest(@Param('requestId') requestId: number) {
     return this.friendService.acceptFriendRequest(requestId);
   }
-
+  @UseGuards(UserGuard)
   @Post('reject/:requestId')
   rejectFriendRequest(@Param('requestId') requestId: number) {
     return this.friendService.rejectFriendRequest(requestId);
