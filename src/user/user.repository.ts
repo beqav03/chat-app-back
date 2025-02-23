f91fc5ba72d35f3ed155767c9b1be97cc7693134
Apmen03 <b.vardy03@gmail.com>
2025-02-23 18:13:53 +0400

    update search function

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -122,7 +122,7 @@ export class UserRepository {
   async searchUsers(keyword: string): Promise<{ name: string, lastname: string, email: string }[]> {
     const users = await this.userRepo
       .createQueryBuilder('user')
-      .select(['user.name', 'user.lastname', 'user.email'])
+      .select(['user.id','user.name', 'user.lastname', 'user.email'])
       .where('user.name LIKE :keyword', { keyword: `%${keyword}%` })
       .orWhere('user.lastname LIKE :keyword', { keyword: `%${keyword}%` })
       .orWhere('user.email LIKE :keyword', { keyword: `%${keyword}%` })
