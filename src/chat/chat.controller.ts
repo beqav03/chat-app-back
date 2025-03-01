cd360b1903dcbb9416df5090888dc6069551c99f
Apmen03 <b.vardy03@gmail.com>
2025-03-01 15:49:22 +0400

    add guard

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Post, Body, UseGuards, Get, Param, Query, InternalServerErrorException } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post('send')
+  @UseGuards(JwtGuard)
   sendMessage(@Body() messageDto: { userId: number, message: string, friendId: number }) {
     return this.chatService.sendMessage(messageDto.userId, messageDto.message, messageDto.friendId);
   }
 
   @Get('history/:friendId')
+  @UseGuards(JwtGuard)
   async getChatHistory(@Query('userId') userId: string, @Param('friendId') friendId: string) {
     try {
       const history = await this.chatService.getChatHistory(+userId, +friendId);
