aee84456ec44dc96ce137885c340f0f559426237
Apmen03 <b.vardy03@gmail.com>
2025-03-19 20:39:43 +0400

    try to fix

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = [
-    process.env.FRONTEND_URL
+    process.env.FRONTEND_URL, 
+    "https://www.back-end.com.ge"
   ];
-  
+
   app.enableCors({
     origin: (origin, callback) => {
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
+        callback(null, true); 
       } else {
         callback(new Error('Not allowed by CORS'));
       }
     },
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Authorization'
   });
 
   await app.listen(3000);
